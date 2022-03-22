cb63960df87755c17318c50ac0304694ff03b00d
Andrew Avola <76986167+Meatballin@users.noreply.github.com>
2022-03-21 21:13:25 -0700

    Sprint 2 Commit 2
    Implemented:
    -Weapon Swap Functionality
    -Audio Manager
    -Rocket Launcher
    -Rocket Sound Effect Clips
    
    Bugs:
    -sometimes rifle bullets make it through target. Not sure why this occurs

--- TINC Game/Assets/Weapon.cs
+++ TINC Game/Assets/Weapon.cs
@@ -1,47 +1,74 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-
+    private GameObject referenceToAudio;
     public Transform firePoint;
     public Camera gameCamera;
+    private int currentWeapon;
 
     //RIFLE CODE
     public GameObject rifleMuzzleFlash;
-    public GameObject bulletPreFab;
+    public GameObject bulletPrefab;
     private bool isFlashing = false;
-    private float rifleFireRate = 0.1f;
+    private float rifleFireRate = 0.13f;
     private float framesFlashed;
     private float maxFlashFrames = 4.7f;
     private float lastShot = 0.0f;
-    [SerializeField] private AudioSource source;
+    /*[SerializeField] private AudioSource source;
     [SerializeField] private AudioClip rifleBulletSound;
-    [SerializeField] private AudioClip rifleLastBulletSound;
+    [SerializeField] private AudioClip rifleLastBulletSound;*/
+
+    //ROCKET LAUNCHER CODE
+    public GameObject rocketPrefab;
+    private float rpgFireRate = 0.35f;
+   /* [SerializeField] private AudioClip rocketSound;*/
     
     private void Start()
     {
+        referenceToAudio = GameObject.Find("AudioManager");
+        //Always start game with m4
+        currentWeapon = 1;
+        //Makes sure muzzle flash is off on game start
         rifleMuzzleFlash.SetActive(false);
+
     }
     // Update is called once per frame
     private void Update()
     {
-        AimHandler();
-        if (Input.GetButton("Fire1"))
+        //Function to handle weapon swapping
+        weaponSwaps();
+        //Handles which weapon we have equipped
+        switch(currentWeapon)
         {
-            RifleShoot();
-            
-            if(!isFlashing)
-            {
-                source.PlayOneShot(rifleBulletSound);
-                StartCoroutine(RifleMuzzleFlash());
-            }
+            case 1:
+                RifleProfile();
+                break;
+            case 2:
+                RPGProfile();
+                break;
             
         }
-        if(Input.GetButtonUp("Fire1"))
+       
+    }
+
+    private void weaponSwaps()
+    {
+        //Handles weapon swaps depending on 'Q' or 'E' press
+        if (Input.GetKeyDown(KeyCode.Q))
         {
-            source.PlayOneShot(rifleLastBulletSound);
+            if (currentWeapon != 1)
+                currentWeapon--;
+            if (currentWeapon == 1)
+                currentWeapon = 1;
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (currentWeapon != 3)
+                currentWeapon++;
+            if (currentWeapon == 3)
+                currentWeapon = 3;
         }
     }
 
@@ -51,11 +78,62 @@ public class Weapon : MonoBehaviour
         if (Time.time > rifleFireRate + lastShot)
         {
 
-            Instantiate(bulletPreFab, firePoint.position, firePoint.rotation);
+            Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
             lastShot = Time.time;
 
         }
 
+    }
+
+    private void RifleProfile()
+    {
+        transform.GetChild(1).GetChild(1).gameObject.SetActive(false);
+        transform.GetChild(1).GetChild(0).gameObject.SetActive(true);
+        
+
+        AimHandler();
+        if (Input.GetButton("Fire1"))
+        {
+            RifleShoot();
+
+            if (!isFlashing)
+            {
+                /*source.PlayOneShot(rifleBulletSound);*/
+                FindObjectOfType<AudioManager>().Play("RifleBulletSound");
+                StartCoroutine(RifleMuzzleFlash());
+            }
+
+        }
+        if (Input.GetButtonUp("Fire1"))
+        {
+            /*source.PlayOneShot(rifleLastBulletSound);*/
+            FindObjectOfType<AudioManager>().Play("RifleEndSound");
+        }
+    }
+
+    private void RPGProfile()
+    {
+        //Turns off M4 and brings in RPG
+        transform.GetChild(1).GetChild(0).gameObject.SetActive(false);
+        transform.GetChild(1).GetChild(1).gameObject.SetActive(true);
+        
+        AimHandler();
+        if(Input.GetButtonDown("Fire1"))
+        {
+            RPGShoot();
+        }
+    }
+    private void RPGShoot()
+    {
+        if (Time.time > rpgFireRate + lastShot)
+        {
+
+            Instantiate(rocketPrefab, firePoint.position, firePoint.rotation);
+            FindObjectOfType<AudioManager>().Play("RocketLaunch");
+            /*source.PlayOneShot(rocketSound);*/
+            lastShot = Time.time;
+            
+        }
     }
 
     private void AimHandler()
