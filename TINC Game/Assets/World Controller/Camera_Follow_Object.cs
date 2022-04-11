59deefdd74df665f40ee39e3f7fba70688d800e5
Doctor Laplace <silvervalifor@gmail.com>
2022-04-11 09:18:58 -0700

    Added physics impulse object, enemy scuttlers, and bullet entity interaction

--- TINC Game/Assets/World Controller/Camera_Follow_Object.cs
+++ TINC Game/Assets/World Controller/Camera_Follow_Object.cs
@@ -17,11 +17,13 @@ public class Camera_Follow_Object : MonoBehaviour
 
     void FixedUpdate ()
     {
-        Vector3 playerPosition = new Vector3(player.position.x, player.position.y, -10);
-        Vector3 finalPosition = player.position + cameraOffset;
-        Vector3 lerpPosition = Vector3.Lerp (transform.position, finalPosition, cameraSpeed);
-        Vector3 lerpFlatten = new Vector3(lerpPosition.x, lerpPosition.y, -10);
+        if (player != null){
+            Vector3 playerPosition = new Vector3(player.position.x, player.position.y, -10);
+            Vector3 finalPosition = player.position + cameraOffset;
+            Vector3 lerpPosition = Vector3.Lerp (transform.position, finalPosition, cameraSpeed);
+            Vector3 lerpFlatten = new Vector3(lerpPosition.x, lerpPosition.y, -10);
 
-        transform.position = lerpFlatten;
+            transform.position = lerpFlatten;
+        }
     }
 }
