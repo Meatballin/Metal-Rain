1a01aeda4fb87d8a29eed6904a61458e8c62844c
Doctor Laplace <silvervalifor@gmail.com>
2022-04-11 21:13:17 -0700

    Minor changes to hazards

---
+++ TINC Game/Assets/Hazard_Collision.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hazard_Collision : MonoBehaviour
+{
+    public float hazardDamage = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision){
+        if (collision.gameObject.CompareTag("Player")){
+            collision.gameObject.GetComponent<Entity>().ApplyDamage(hazardDamage);
+        }
+    } 
+}
